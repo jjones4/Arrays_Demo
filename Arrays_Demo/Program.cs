9d96d530a81d94b3985ea45f523c5b0a01f81d03
Alma <alma@alma-c-box.local>
2022-11-26 10:52:54 -0600

    Add Example Code for a Single-Dimensional Array

--- Arrays_Demo/Program.cs
+++ Arrays_Demo/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*
+             * 
+             * Single-dimensional array
+             * Compute the powers of two with exponents
+             * ranging from 0 to 63
+             * 
+             */
+
+            // Instantiate and fill the array with the
+            // powers of two
+            ulong[] powersOfTwo = new ulong[64];
+
+            for (int i = 0; i < powersOfTwo.Length; i++)
+            {
+                powersOfTwo[i] = (ulong)Math.Pow(2, i);
+            }
+
+            // Write the powers of two to the console
+            int exponenet = 0;
+
+            foreach (ulong result in powersOfTwo)
+            {
+                Console.WriteLine($"Exponenet: {exponenet}: Result: {powersOfTwo[exponenet]}");
+
+                exponenet++;
+            }
         }
     }
 }
